eef1c5b62acdc563c4cdba26d089ad2cabd9843b
Ali Bouhrouche <ali@iev.ma>
2025-03-14 16:32:14 +0000

    Use the frame named Cover, if it exists, or use the first page to generate thumbnails.

--- src/components/board/app-thumbnail.tsx
+++ src/components/board/app-thumbnail.tsx
@@ -1,7 +1,39 @@
-import { useEditor } from "tldraw";
+import { Box, Editor, TLFrameShape, TLShapeId, useEditor } from "tldraw";
 import { useEffect } from "react";
 import { useBoardId } from ".";
 import { db } from "@/db";
+
+function getCoverFrame(editor: Editor) {
+  const shapeIds = editor.store.query.exec("shape", {
+    type: { eq: "frame" },
+  }) as TLFrameShape[];
+  const coverFrame = shapeIds.find(
+    (shape) => shape.props.name.toLowerCase() === "cover"
+  );
+  return coverFrame;
+}
+
+function getCoverShapes(editor: Editor, coverFrame?: TLFrameShape) {
+  let shapes: Set<TLShapeId>;
+  if (coverFrame) {
+    const coverShapesIds = editor.getShapeAndDescendantIds([coverFrame.id]);
+    coverShapesIds.delete(coverFrame.id);
+    shapes = coverShapesIds;
+  } else {
+    shapes = editor.getPageShapeIds(editor.getPages()[0]);
+  }
+  return Array.from(shapes).map((id) => editor.getShape(id)!);
+}
+
+function getCoverBounds(coverFrame?: TLFrameShape) {
+  if (!coverFrame) return;
+  const {
+    x,
+    y,
+    props: { w, h },
+  } = coverFrame;
+  return new Box(x, y, w, h);
+}
 
 export default function AppThumbnail() {
   const editor = useEditor();
@@ -11,11 +43,14 @@ export default function AppThumbnail() {
     let timeout: number;
     let cancelled = false;
     async function screenshot() {
-      const shapes = editor.getCurrentPageShapes();
+      const coverFrame = getCoverFrame(editor);
+      const shapes = getCoverShapes(editor, coverFrame);
+      const bounds = getCoverBounds(coverFrame);
       const svg =
         shapes.length > 0
           ? await editor.getSvgString(shapes, {
               darkMode: false,
+              bounds,
             })
           : {
               svg: `<svg xmlns="http://www.w3.org/2000/svg" width="1920" height="1080" fill="none"><path fill="#fff" d="M0 0h1920v1080H0z"/></svg>`,
